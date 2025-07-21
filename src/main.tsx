ca5b3e10137e41cfcac3a0aa998fec2b4400f07a
Bernardo Marques <bernardohmarques@gmail.com>
2025-07-20 21:55:42 -0300

    chore: add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./config/query-client.ts";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <SpeedInsights />
     </QueryClientProvider>
   </StrictMode>
 );
